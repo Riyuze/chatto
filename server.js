ff4fd84892b9f2b3d2334634031fb3f115f618ea
Kevin Tandavo <riyuze211001@gmail.com>
2022-10-07 15:49:18 +0700

    Added static files

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
+
+//Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = 3000 || process.env.PORT;
 
