ead25c6f16eeed86a6ea98614d7ecdb8d2c9b7ac
Kevin Tandavo <riyuze211001@gmail.com>
2022-10-07 18:19:41 +0700

    Added chat boxes

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,19 @@
 const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.class-messages');
 
 const socket = io();
 
+// Message from Server
 socket.on('message', message => {
     console.log(message);
+    outputMessage(message);
+
+    // Scrolls Down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 // Message Submit
-chatForm.addEventListener('submit', (e) => {
+chatForm.addEventListener('submit', e => {
     e.preventDefault();
 
     // Get Message Text
@@ -15,4 +21,19 @@ chatForm.addEventListener('submit', (e) => {
 
     // Emit a Message to the Server
     socket.emit('chatMessage', msg);
+
+    // Clear Input
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
 });
+
+// Output Message to DOM
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+                        <p class="text">
+                            ${message}
+                        </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+}
