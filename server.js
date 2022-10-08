70548095278cfecfd4f1a4ff3553f01a54c465fe
Kevin Tandavo <riyuze211001@gmail.com>
2022-10-08 22:42:53 +0700

    Removed hard codes

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+const botName = 'Chatto Bot';
 
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,19 +16,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Run When Client Connects
 io.on('connection', socket => {
     // Welcomes the Current User
-    socket.emit('message', 'Welcome to Chatto!');
+    socket.emit('message', formatMessage(botName, 'Welcome to Chatto!'));
 
     // Broadcasts When a User Connects
-    socket.broadcast.emit('message', 'A user has joined the chat');
+    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
 
     // Broadcasts When a User Disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat');
+        io.emit('message', formatMessage(botName, 'A user has left the chat'));
     });
 
     // Listen for Chat Messages
     socket.on('chatMessage', (msg) => {
-        io.emit('message', msg);
+        io.emit('message', formatMessage('User', msg));
     });
 });
 
