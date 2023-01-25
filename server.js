4730dbd706993cc79ec4e3dc77822403c7318f21
Kevin Tandavo <riyuze211001@gmail.com>
2023-01-25 14:42:50 +0700

    Fixed typo

--- server.js
+++ server.js
@@ -51,6 +51,6 @@ io.on('connection', socket => {
     });
 });
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Server Running on Port ${PORT}`));
