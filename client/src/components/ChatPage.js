2654266552f8948655f6f4b944420c1e4948c279
Kevin Tandavo <riyuze211001@gmail.com>
2023-01-26 18:58:00 +0700

    Added tailwind css, started making chat page

--- client/src/components/ChatPage.js
+++ client/src/components/ChatPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Text } from '@nextui-org/react';
 
 class ChatPage extends React.Component {
 
@@ -12,7 +13,27 @@ class ChatPage extends React.Component {
     render () {
         return (
             <div className="ChatPage">
-                <p>Chat</p>
+                <div>
+                    <Text h2 css={{
+                        textGradient: "45deg, $blue600 50%, $blue500 50%",
+                        padding: "20px",
+                    }}
+                    weight="bold">Chatto</Text>
+                    <Text h4 css={{
+                        textGradient: "45deg, $blue700 -20%, $blue800 100%",
+                        margin: "30px 0 20px 0",
+                        }}
+                    weight="bold">Active Users</Text>
+                    <div>
+                        <p>User 1</p>
+                        <p>User 2</p>
+                        <p>User 3</p>
+                        <p>User 4</p>
+                    </div>
+                </div>
+                <div>
+                    
+                </div>
             </div>
         )
     }
