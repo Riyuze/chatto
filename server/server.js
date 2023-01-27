8ea02b5fb37225857b3b0cc158c08a532bb61b0e
Kevin Tandavo <riyuze211001@gmail.com>
2023-01-27 12:48:28 +0700

    Reworked server

--- server/server.js
+++ server/server.js
@@ -1,59 +1,32 @@
-const path = require('path');
-const http = require('http');
-const cors = require('cors');
 const express = require('express');
-const socketio = require('socket.io');
-const formatMessage = require('./utils/messages');
-const {userJoin, getCurrentUser, userLeave, getRoomUsers} = require('./utils/users')
-
 const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
+const PORT = 4000;
 
-const botName = 'Chatto Bot';
+const http = require('http').Server(app);
+const cors = require('cors');
+
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: 'http://localhost:3000'
+    }
+})
 
 app.use(cors());
 
-// Set Static Folder
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// Run When Client Connects
-io.on('connection', socket => {
-    socket.on('joinRoom', ({username, room}) => {
-        const user = userJoin(socket.id, username, room);
-
-        socket.join(user.room);
-
-        // Welcomes the Current User
-        socket.emit('message', formatMessage(botName, 'Welcome to Chatto!'));
-
-        // Broadcasts When a User Connects
-        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`));
-
-        // Sends Users and Chatroom Info
-        io.to(user.room).emit('roomUsers', {room: user.room, users: getRoomUsers(user.room)});
-    });
-
-    // Listen for Chat Messages
-    socket.on('chatMessage', (msg) => {
-        const user = getCurrentUser(socket.id);
- 
-        io.to(user.room).emit('message', formatMessage(user.username, msg));
-    });
- 
-    // Broadcasts When a User Disconnects
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
     socket.on('disconnect', () => {
-        const user = userLeave(socket.id);
-
-        if(user) {
-            io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`));
-
-            // Sends Users and Chatroom Info
-            io.to(user.room).emit('roomUsers', {room: user.room, users: getRoomUsers(user.room)});
-        }
+      console.log('🔥: A user disconnected');
     });
 });
 
-const PORT = process.env.PORT || 4000;
+app.get('/api', (req, res) => {
+    res.json({
+        message: "Hello World",
+    });
+});
 
-server.listen(PORT, () => console.log(`Server Running on Port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
+
